0803fa23b3c576a90d9dab3c62891bea6a54748e
Jay Phillips <jicksta+git@gmail.com>
2012-03-25 14:25:57 -0700

    CLI usability improvements.

--- cli.js
+++ cli.js
@@ -1,12 +1,32 @@
-var _ = require("underscore");
-var Engine = require("./src/engine.js");
+var _ = require("underscore"),
+    colors = require('colors');
 
-repl = require("repl").start("RetroQuest >");
-_.extend(repl.context, Engine);
+var Engine = require("./src/engine.js");
 
 var world = new Engine.World;
 var player = new Engine.Player(world);
 
-repl.context.world = world;
-repl.context.player = player;
+var TICK_TIME_MS = 6000;
 
+setupREPL();
+
+function setupREPL() {
+  console.log("Starting RetroQuest CLI. Take a look at your " + "world".yellow + " and " + "player".yellow);
+
+  var repl = require("repl").start("RetroQuest > ");
+
+  _.extend(repl.context, Engine);
+
+  repl.context.world = world;
+  repl.context.player = player;
+
+  setInterval(function() {
+    world.tick();
+  }, TICK_TIME_MS);
+
+  repl.rli.on('close', function() {
+    console.log("\nYou set up camp and fall asleep.".green);
+    process.exit();
+  });
+
+}
