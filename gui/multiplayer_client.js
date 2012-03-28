035b86c28b4eeacf55dd3b48594484b75cb7aeab
Jay Phillips <jicksta+git@gmail.com>
2012-03-27 23:23:48 -0700

    Adding rudimentary 2D multiplayer to flesh out the state synchronization stack. Also made the ThreeJS collada loader gui.html

--- gui/multiplayer_client.js
+++ gui/multiplayer_client.js
@@ -1,7 +1,84 @@
 $(function() {
+
+  var MAP_WIDTH = 1000,
+      MAP_HEIGHT = 500,
+      GRID_SIZE = 10;
+
+  var map = Raphael("map", MAP_WIDTH, MAP_HEIGHT);
+  var mapOffset = $("#map").offset();
+
+  var player;
+  var otherPlayers = {},
+      playerElements = {};
+
   var socket = io.connect("http://localhost");
-  socket.on("news", function(data) {
-    $("<li/>").text(JSON.stringify(data)).appendTo("#messages");
-    socket.emit("my other event", {my: "data"});
+
+  socket.on("world", function(world) {
+    otherPlayers = $.each(world, function(p) {
+      otherPlayers[p.id] = p;
+    });
+
+    console.log(world);
+    player = createPlayer();
+    socket.emit("create", player);
+
+    drawWorld(world);
+    drawPlayer(player);
+    bindInteraction();
   });
+
+  socket.on("update", function(playerData) {
+    $.extend(otherPlayers[playerData.id], playerData);
+    drawPlayer(playerData);
+  });
+
+  function drawGrid() {
+    map.rect(0, 0, MAP_WIDTH, MAP_HEIGHT);
+    for (var i = 0; i < MAP_WIDTH; i += GRID_SIZE) {
+      map.path(Raphael.format("M{0} 0L{0} {1}", i, MAP_HEIGHT)).attr({stroke:"#ccc"});
+    }
+    for (var i = 0; i < MAP_HEIGHT; i += GRID_SIZE) {
+      map.path(Raphael.format("M0 {0}L{1} {0}", i, MAP_WIDTH)).attr({stroke:"#ccc"});
+    }
+  }
+
+  function createPlayer() {
+    return {
+      id:Math.random(),
+      x:random(MAP_WIDTH),
+      y:random(MAP_HEIGHT),
+      color:Raphael.rgb(random(255), random(255), random(255))
+    }
+  }
+
+  function drawPlayer(player) {
+    if (player.id in playerElements) {
+      playerElements[player.id].remove();
+    }
+    playerElements[player.id] = map.circle(player.x, player.y, 10).attr({fill:player.color});
+  }
+
+  function bindInteraction() {
+    $("#map").mousemove(function(e) {
+      updateEvent(e);
+    });
+    function updateEvent(e) {
+      player.x = e.pageX - mapOffset.left;
+      player.y = e.pageY - mapOffset.top;
+      drawPlayer(player);
+      socket.emit("update", player);
+    }
+  }
+
+  function drawWorld() {
+    drawGrid();
+    $.each(otherPlayers, function(id, player) {
+      drawPlayer(player);
+    });
+  }
+
+  function random(n) {
+    return Math.random() * random
+  }
+
 });
--- multiplayer_server.js
+++ multiplayer_server.js
@@ -1,29 +1,53 @@
-var util = require('util'),
-    connect = require('connect'),
-    port = 1337;
-
-var STATIC_DIR = __dirname + "/gui/";
-
-// var staticServer = connect.createServer(STATIC_DIR).listen(port);
-
-// util.puts('Listening on ' + port + '...');
-// util.puts('Press Ctrl + C to stop.');
+var connect = require('connect'),
+    _ = require("underscore"),
+    port = 3000;
 
 var app = connect()
-  .use(connect.logger('dev'))
-  .use(connect.static('gui'))
-  .use(function(req, res){
-    res.end('hello world\n');
-  })
- .listen(3000);
+    .use(connect.logger('dev'))
+    .use(connect.static('gui'))
+    .use(function(req, res) {
+      res.end("Couldn't find that.\n");
+    })
+    .listen(port);
 
 var io = require("socket.io").listen(app);
 
-// util.puts("Serving files from " + STATIC_DIR);
+var game = new Game();
 
 io.sockets.on("connection", function(socket) {
-  socket.emit("news", {hello: "world"});
-  socket.on("my other event", function(data) {
-    console.log(data);
+  game.playerConnected(socket);
+  socket.on("update", function(player) {
+    game.movePlayer(player);
   });
 });
+
+
+function Game() {
+  var players = [];
+  var playerSockets = {};
+
+  this.playerConnected = function(socket) {
+    socket.emit("world", players);
+    socket.on("create", function(playerData) {
+      game.addPlayer(playerData, socket);
+    });
+  };
+
+  this.addPlayer = function(playerData, socket) {
+    players.push(playerData);
+    playerSockets[playerData.id] = socket;
+  };
+
+  this.movePlayer = function(playerData) {
+    for (var i in players) {
+      var thisPlayer = players[i];
+      if (thisPlayer.id == playerData.id) {
+        _.extend(thisPlayer, playerData);
+      } else {
+        var thisPlayerSocket = playerSockets[thisPlayer.id];
+        thisPlayerSocket.emit("update", playerData);
+      }
+    }
+  }
+
+}
